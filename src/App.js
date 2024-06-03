5a66e4c6a150e0f2563dba01b55e8c8fdfcfbe0a
hoan <hoandvx@gmail.com>
2024-06-03 20:26:47 +0700

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar.tsx';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import Admin from './pages/Admin.tsx'
+import Static from './pages/Static.tsx'
+import Article from './pages/Article.tsx'
+import PD from './pages/PD.tsx'
+import Categories from './pages/Categories.tsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Sidebar/>
+        <Routes>
+          <Route path='/static' element={<Static/>}></Route>
+          <Route path='/admin' element={<Admin/>}></Route>
+          <Route path='/article' element={<Article/>}></Route>
+          <Route path='/pd' element={<PD/>}></Route>
+          <Route path='/categories' element={<Categories/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
+      
 }
 
 export default App;
